9f70b716d727a292fe97cbda5bd23fab5553519f
pmikola <pio.mikolajczyk@gmail.com>
2022-08-07 21:33:00 +0200

    GAN from - https://arxiv.org/pdf/1511.06434.pdf before upgrades + DATALOADER ISSUE

--- GAN.py
+++ GAN.py
@@ -1,27 +1,11 @@
-import torch
 import torch.nn as nn
-
-# %matplotlib inline
-import argparse
-import os
-import random
-import torch
-import torch.nn as nn
-import torch.nn.parallel
-import torch.backends.cudnn as cudnn
-import torch.optim as optim
-import torch.utils.data
-import torchvision.datasets as dset
-import torchvision.transforms as transforms
-import torchvision.utils as vutils
 
 
 # DCGAN ARCHITECTURE https://arxiv.org/pdf/1511.06434.pdf
 
 # Generator Code
-
 class Generator(nn.Module):
-    def __init__(self, ngpu,nz,ngf,nc):
+    def __init__(self, ngpu, nz, ngf, nc):
         super(Generator, self).__init__()
         self.nz = nz
         self.ngf = ngf
@@ -29,7 +13,7 @@ class Generator(nn.Module):
         self.nc = nc
         self.main = nn.Sequential(
             # input is Z, going into a convolution
-            nn.ConvTranspose2d( self.nz, self.ngf * 8, 4, 1, 0, bias=False),
+            nn.ConvTranspose2d(self.nz, self.ngf * 8, 4, 1, 0, bias=False),
             nn.BatchNorm2d(self.ngf * 8),
             nn.ReLU(True),
             # state size. (ngf*8) x 4 x 4
@@ -37,15 +21,15 @@ class Generator(nn.Module):
             nn.BatchNorm2d(self.ngf * 4),
             nn.ReLU(True),
             # state size. (ngf*4) x 8 x 8
-            nn.ConvTranspose2d( self.ngf * 4, self.ngf * 2, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(self.ngf * 4, self.ngf * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(self.ngf * 2),
             nn.ReLU(True),
             # state size. (ngf*2) x 16 x 16
-            nn.ConvTranspose2d( self.ngf * 2, self.ngf, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(self.ngf * 2, self.ngf, 4, 2, 1, bias=False),
             nn.BatchNorm2d(self.ngf),
             nn.ReLU(True),
             # state size. (ngf) x 32 x 32
-            nn.ConvTranspose2d( self.ngf, self.nc, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(self.ngf, self.nc, 4, 2, 1, bias=False),
             nn.Tanh()
             # state size. (nc) x 64 x 64
         )
@@ -54,8 +38,9 @@ class Generator(nn.Module):
         return self.main(input)
 
 
+# Discriminator Code
 class Discriminator(nn.Module):
-    def __init__(self, ngpu,ndf,nc):
+    def __init__(self, ngpu, ndf, nc):
         super(Discriminator, self).__init__()
         self.ngpu = ngpu
         self.ndf = ndf
--- main.py
+++ main.py
@@ -72,7 +72,7 @@ lr = 0.0002
 beta1 = 0.5
 # Number of GPUs available. Use 0 for CPU mode.
 ngpu = 1
-show_training_set = True
+show_training_set = False
 # np.random.seed(2022)
 # np.random.seed(828)
 # torch.manual_seed(2022)
@@ -120,10 +120,9 @@ if show_training_set == True:
     plt.axis("off")
     ims = []
     axes = []
-
+    ims_tmp = []
     for i in range(0, len(names_struct)):
         T = 0
-        ims_tmp = []
         if i < 5:
             ax = fig.add_subplot(grid[0:49, 20 * i:20 * i + 20])
         else:
@@ -139,6 +138,7 @@ if show_training_set == True:
                                      extent=[0, grid_size * metas_tensor[i][13], 0, grid_size * metas_tensor[i][13]],
                                      padding=2, normalize=True).cpu(), (1, 2, 0)))
                 ims.append([ims_fields])
+
     ani = animation.ArtistAnimation(fig, ims, interval=30, blit=True)
     plt.show()
 
@@ -165,7 +165,8 @@ else:
     pass
 
 
-# custom weights initialization called on netG and netD
+# custom weights initialization called on netG and netD from random distribution with
+# mean = 0 | and stdev = 0.02
 def weights_init(m):
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
@@ -202,3 +203,111 @@ netD.apply(weights_init)
 
 # Print the model
 print(netD)
+
+
+# Initialize BCELoss function
+criterion = nn.BCELoss()
+
+# Create batch of latent vectors that we will use to visualize
+#  the progression of the generator
+fixed_noise = torch.randn(64, nz, 1, 1, device=device)
+
+# Establish convention for real and fake labels during training
+real_label = 1.
+fake_label = 0.
+
+# Setup Adam optimizers for both G and D
+optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
+optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
+
+
+# Training Loop
+
+# Lists to keep track of progress
+img_list = []
+G_losses = []
+D_losses = []
+iters = 0
+
+print("Starting Training Loop...")
+# For each epoch
+for epoch in range(num_epochs):
+    # For each batch in the dataloader
+    for i, data in enumerate(dataloader_fields, 0):
+
+        ############################
+        # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+        ###########################
+        ## Train with all-real batch
+        netD.zero_grad()
+        # Format batch
+        real_cpu = data[0].to(device)
+        b_size = real_cpu.size(0)
+        label = torch.full((b_size,), real_label, dtype=torch.float, device=device)
+        # Forward pass real batch through D
+        output = netD(real_cpu).view(-1)
+        # Calculate loss on all-real batch
+        errD_real = criterion(output, label)
+        # Calculate gradients for D in backward pass
+        errD_real.backward()
+        D_x = output.mean().item()
+
+        ## Train with all-fake batch
+        # Generate batch of latent vectors
+        noise = torch.randn(b_size, nz, 1, 1, device=device)
+        # Generate fake image batch with G
+        fake = netG(noise)
+        label.fill_(fake_label)
+        # Classify all fake batch with D
+        output = netD(fake.detach()).view(-1)
+        # Calculate D's loss on the all-fake batch
+        errD_fake = criterion(output, label)
+        # Calculate the gradients for this batch, accumulated (summed) with previous gradients
+        errD_fake.backward()
+        D_G_z1 = output.mean().item()
+        # Compute error of D as sum over the fake and the real batches
+        errD = errD_real + errD_fake
+        # Update D
+        optimizerD.step()
+
+        ############################
+        # (2) Update G network: maximize log(D(G(z)))
+        ###########################
+        netG.zero_grad()
+        label.fill_(real_label)  # fake labels are real for generator cost
+        # Since we just updated D, perform another forward pass of all-fake batch through D
+        output = netD(fake).view(-1)
+        # Calculate G's loss based on this output
+        errG = criterion(output, label)
+        # Calculate gradients for G
+        errG.backward()
+        D_G_z2 = output.mean().item()
+        # Update G
+        optimizerG.step()
+
+        # Output training stats
+        if i % 50 == 0:
+            print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+                  % (epoch, num_epochs, i, len(dataloader_fields),
+                     errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+
+        # Save Losses for plotting later
+        G_losses.append(errG.item())
+        D_losses.append(errD.item())
+
+        # Check how the generator is doing by saving G's output on fixed_noise
+        if (iters % 500 == 0) or ((epoch == num_epochs-1) and (i == len(dataloader_fields)-1)):
+            with torch.no_grad():
+                fake = netG(fixed_noise).detach().cpu()
+            img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+
+        iters += 1
+
+plt.figure(figsize=(10,5))
+plt.title("Generator and Discriminator Loss During Training")
+plt.plot(G_losses,label="G")
+plt.plot(D_losses,label="D")
+plt.xlabel("iterations")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
