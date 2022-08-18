0169c13e7b9b78d5d03596d7d1c193f6c7861963
pmikola <pio.mikolajczyk@gmail.com>
2022-08-18 11:17:06 +0200

    GAN Optimisation - added better noise input and G activation function for given data

--- main.py
+++ main.py
@@ -64,8 +64,8 @@ nc = 1
 # Size of z latent vector (i.e. size of generator input)
 nz = 100  # 100
 # Range of the latent vector values
-r_min = -1
-r_max = 1
+r_min = -2
+r_max = 2
 # Size of feature maps in generator
 ngf = 250
 # Size of feature maps in discriminator
@@ -73,10 +73,15 @@ ndf = 250
 # Number of training epochs
 num_epochs = 100
 # Learning rate for optimizers
-lr = 0.0005
+lr = 0.0004
 # Beta1 and beta2 hyperparam for Adam optimizers
 beta1 = 0.95
 beta2 = 0.99
+# momentum for RMSprop optimizers
+momentumG = 0.95
+momentumD = 0.95
+# Weight clipping values (1 means no clipping)
+clip = 0.01
 # Number of GPUs available. Use 0 for CPU mode.
 ngpu = 1
 # Show shapes of Gen and Disc in and out
@@ -112,7 +117,6 @@ def weights_init(m):
     elif classname.find('BatchNorm') != -1:
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
-
 
 # ------------------------------- FUNC --------------------------
 # ------------------------------- TRAIN -------------------------
@@ -273,38 +277,29 @@ for lap_counter in range(0, laps):
 
     # Initialize Loss function
     criterion = nn.BCELoss()
-    # criterion = nn.NLLLoss()
-    # criterion = nn.SmoothL1Loss()
-    # criterion = nn.BCEWithLogitsLoss()
-    # criterion = nn.MSELoss()
-    # criterion = nn.HingeEmbeddingLoss()
-    # Create batch of latent vectors that we will use to visualize
-    #fixed_noise = torch.randn(batch_size, nz, nz, device=device)
+    # Fixed noise visoalisation
     fixed_noise = torch.randn(batch_size, nz, nz, device=device)
 
     # Establish convention for real and fake labels during training
     real_label = 1.
     fake_label = 0.
 
-    # Setup Adam optimizers for both G and D
-    # if lap_counter == 0:
-
-    # optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, beta2), eps=1e-08, weight_decay=0, )
-    # optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, beta2), eps=1e-08, weight_decay=0, )
+    # optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, beta2), eps=1e-08, weight_decay=0 )
+    # optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, beta2), eps=1e-08, weight_decay=0 )
     # optimizerD = optim.SGD(netD.parameters(), lr=lr, momentum=0.9)
     # optimizerG = optim.SGD(netG.parameters(), lr=lr, momentum=0.9)
     # else:
     # optimizerG = optim.RMSprop(netG.parameters(), lr=5e-5)
     # optimizerD = optim.RMSprop(netD.parameters(), lr=5e-5)
-    optimizerD = optim.RMSprop(netD.parameters(), lr=lr, alpha=0.9, eps=1e-09, weight_decay=0, momentum=0.95)
-    optimizerG = optim.RMSprop(netG.parameters(), lr=lr, alpha=0.9, eps=1e-09, weight_decay=0, momentum=0.95)
+    optimizerD = optim.RMSprop(netD.parameters(), lr=lr, alpha=0.9, eps=1e-09, weight_decay=0, momentum=momentumD)
+    optimizerG = optim.RMSprop(netG.parameters(), lr=lr, alpha=0.9, eps=1e-09, weight_decay=0, momentum=momentumG)
     # optimizerD = optim.Adagrad(netD.parameters(), lr=lr, lr_decay=0.1, weight_decay=0.1,
     # initial_accumulator_value=0.1, eps=1e-10) optimizerG = optim.Adagrad(netG.parameters(), lr=lr, lr_decay=0.1,
     # weight_decay=0.15, initial_accumulator_value=0.1, eps=1e-10)
 
     iters = 0
     if lap_counter == 0:
-        fakes_loss_modes = plt.figure(figsize=(18, 6))
+        fakes_loss_modes = plt.figure(figsize=(18, 8))
         grid = plt.GridSpec(100, 100, wspace=0.5, hspace=0.5)
         for i in range(0, 20):
             if i < 5:
@@ -366,9 +361,10 @@ for lap_counter in range(0, laps):
             # Calculate loss on all-real batch
             errD_real = criterion(output, label)
             # Calculate gradients for D in backward pass
+            # errD_real.backward()
+            # D_x = output.mean().item()
             errD_real.backward()
             D_x = output.mean().item()
-
             ## Train with all-fake batch
             # Generate batch of latent vectors
 
@@ -402,13 +398,18 @@ for lap_counter in range(0, laps):
             # Calculate the gradients for this batch, accumulated (summed) with previous gradients
             errD_fake.backward()
             D_G_z1 = output.mean().item()
+
             # Compute error of D as sum over the fake and the real batches
             errD = errD_real + errD_fake
             # Update D
             optimizerD.step()
-            # WGAN change
-            for p in netD.parameters():
-                p.data.clamp_(-0.01, 0.01)
+            if epoch % 10 == 0:
+                pass
+            else:
+                # WGAN change
+                for p in netD.parameters():
+                    p.data.clamp_(-clip, clip)
+
             ############################
             # (2) Update G network: maximize log(D(G(z)))
             ###########################
@@ -439,73 +440,72 @@ for lap_counter in range(0, laps):
             if (iters % 5 == 0) or ((epoch == num_epochs - 1) and (batch_idx == len(dataloader_structures) - 1)):
                 with torch.no_grad():
                     fake = netG(fixed_noise).detach().cpu()
-                fake_learning0 = azes[0].imshow(fake[0])
-                fake_learning1 = azes[1].imshow(fake[1])
-                fake_learning2 = azes[2].imshow(fake[2])
-                fake_learning3 = azes[3].imshow(fake[3])
-                fake_learning4 = azes[4].imshow(fake[4])
-                fake_learning5 = azes[5].imshow(fake[5])
-                fake_learning6 = azes[6].imshow(fake[6])
-                fake_learning7 = azes[7].imshow(fake[7])
-                fake_learning8 = azes[8].imshow(fake[8])
-                fake_learning9 = azes[9].imshow(fake[9])
-                fake_learning10 = azes[10].imshow(fake[20])
-                fake_learning11 = azes[11].imshow(fake[21])
-                fake_learning12 = azes[12].imshow(fake[22])
-                fake_learning13 = azes[13].imshow(fake[23])
-                fake_learning14 = azes[14].imshow(fake[24])
-                fake_learning15 = azes[15].imshow(fake[25])
-                fake_learning16 = azes[16].imshow(fake[26])
-                fake_learning17 = azes[17].imshow(fake[27])
-                fake_learning18 = azes[18].imshow(fake[28])
-                fake_learning19 = azes[19].imshow(fake[29])
-                loss_G, = azes[20].plot(G_losses, color="blue")
-                loss_D, = azes[20].plot(D_losses, color="red")
-                azes[20].set_title("Generator and Discriminator Loss During Training")
-                azes[20].set_xlabel("iterations")
-                azes[20].set_ylabel("Loss")
+                    # fake = fake.detach().cpu()
+                    fake_learning0 = azes[0].imshow(fake[0])
+                    fake_learning1 = azes[1].imshow(fake[1])
+                    fake_learning2 = azes[2].imshow(fake[2])
+                    fake_learning3 = azes[3].imshow(fake[3])
+                    fake_learning4 = azes[4].imshow(fake[4])
+                    fake_learning5 = azes[5].imshow(fake[5])
+                    fake_learning6 = azes[6].imshow(fake[6])
+                    fake_learning7 = azes[7].imshow(fake[7])
+                    fake_learning8 = azes[8].imshow(fake[8])
+                    fake_learning9 = azes[9].imshow(fake[9])
+                    fake_learning10 = azes[10].imshow(fake[10])
+                    fake_learning11 = azes[11].imshow(fake[11])
+                    fake_learning12 = azes[12].imshow(fake[12])
+                    fake_learning13 = azes[13].imshow(fake[13])
+                    fake_learning14 = azes[14].imshow(fake[14])
+                    fake_learning15 = azes[15].imshow(fake[15])
+                    fake_learning16 = azes[16].imshow(fake[16])
+                    fake_learning17 = azes[17].imshow(fake[17])
+                    fake_learning18 = azes[18].imshow(fake[18])
+                    fake_learning19 = azes[19].imshow(fake[19])
+                    loss_G, = azes[20].plot(G_losses, color="blue")
+                    loss_D, = azes[20].plot(D_losses, color="red")
+                    azes[20].set_title("Generator and Discriminator Loss During Training")
+                    azes[20].set_xlabel("iterations")
+                    azes[20].set_ylabel("Loss")
 
-                if y_top == 0 and max(G_losses) > max(D_losses):
-                    y_top = max(G_losses)
-                elif y_top == 0 and max(G_losses) < max(D_losses):
-                    y_top = max(D_losses)
-                else:
-                    pass
+                    if y_top == 0 and max(G_losses) > max(D_losses):
+                        y_top = max(G_losses)
+                    elif y_top == 0 and max(G_losses) < max(D_losses):
+                        y_top = max(D_losses)
+                    else:
+                        pass
 
-                azes[20].text(num_epochs * len(dataloader_structures) - num_epochs * len(dataloader_structures) / 15,
-                              y_top - y_top / 15, 'G', color="blue", fontsize=15.)
-                azes[20].text(num_epochs * len(dataloader_structures) - num_epochs * len(dataloader_structures) / 15,
-                              y_top - 2*(y_top / 15), 'D', color="red",
-                              fontsize=15.)
+                    azes[20].text(
+                        num_epochs * len(dataloader_structures) - num_epochs * len(dataloader_structures) / 15,
+                        y_top - y_top / 10, 'G', color="blue", fontsize=15.)
+                    azes[20].text(
+                        num_epochs * len(dataloader_structures) - num_epochs * len(dataloader_structures) / 15,
+                        y_top - 2 * (y_top / 10), 'D', color="red",
+                        fontsize=15.)
 
-                modes = azes[21].scatter(fake[:, 0],
-                                 fake[:, 1],
-                                 edgecolor='red', facecolor='None', s=5, alpha=1,
-                                 linewidth=1, label='GAN')
-                img.append(
-                    [fake_learning0, fake_learning1, fake_learning2, fake_learning3, fake_learning4, fake_learning5,
-                     fake_learning6, fake_learning7, fake_learning8, fake_learning9, fake_learning10, fake_learning11,
-                     fake_learning12, fake_learning13, fake_learning14, fake_learning15,
-                     fake_learning16, fake_learning17, fake_learning18, fake_learning19, loss_G, loss_D,modes])
-                # img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+                    modesF = azes[21].scatter(fake[:, 0],
+                                              fake[:, 1],
+                                              edgecolor='red', facecolor='None', s=5, alpha=1,
+                                              linewidth=1, label='GAN')
+                    modesR = azes[21].scatter(real[:, 0].cpu(),
+                                              real[:, 1].cpu(),
+                                              edgecolor='blue', facecolor='None', s=5, alpha=1,
+                                              linewidth=1, label='Real')
+                    azes[21].set_title("MODES | Real - blue | Fake - Red")
+                    img.append(
+                        [fake_learning0, fake_learning1, fake_learning2, fake_learning3, fake_learning4, fake_learning5,
+                         fake_learning6, fake_learning7, fake_learning8, fake_learning9, fake_learning10,
+                         fake_learning11,
+                         fake_learning12, fake_learning13, fake_learning14, fake_learning15,
+                         fake_learning16, fake_learning17, fake_learning18, fake_learning19, loss_G, loss_D, modesF,
+                         modesR])
+                    # img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
 
             iters += 1
     if disp_progrss == 1 or lap_counter == laps - 1:
         ani = animation.ArtistAnimation(fakes_loss_modes, img, interval=30, blit=True)
-
         plt.show()
-
-        # plt.figure(figsize=(10, 5))
-        # plt.title("Generator and Discriminator Loss During Training")
-        # plt.plot(G_losses, label="G")
-        # plt.plot(D_losses, label="D")
-        # plt.xlabel("iterations")
-        # plt.ylabel("Loss")
-        # plt.legend()
-        # plt.show()
     else:
         pass
-
     ############## Save Model After Training ###############
     torch.save(netD.state_dict(), pathD)
     torch.save(netG.state_dict(), pathG)
